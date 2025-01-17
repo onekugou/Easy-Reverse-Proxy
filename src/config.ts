2a0b151b805e81cba5ea8b69db7d145ae3a031b3
onekugou <xiaoshouyichuai@outlook.com>
2025-01-17 23:58:37 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,11 @@
-import type {Proxy} from './util/model';
+export interface proxy {
+    domain: string;
+    url: string;
+}
 
-export let PORT = 377; // 端口
-
-export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
-// 代理
-export let PROXYS: Proxy[] = [];
+export let proxys:Proxy[] = [
+    {
+        domain: "easy-reverse-proxy.vercel.app",
+        url: "http://www.google.com/"
+    }     
+];
